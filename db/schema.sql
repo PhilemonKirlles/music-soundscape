d97316a414ac13775657940db93141a38d54419c
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-07-07 16:28:59 -0400

    connecting schema to db

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS music_soundscape_db;
+CREATE DATABASE music_soundscape_db;
---
+++ server.js
@@ -0,0 +1,9 @@
+//setting up express
+
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+
+
+
+
