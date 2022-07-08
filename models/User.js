2fe47c6fe8ba8f653f89441b8c9abc65c02b2f2e
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-07-08 15:17:02 -0400

    origin modle backend file complete

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/routes');
 
 // create our User model
 class User extends Model {}
