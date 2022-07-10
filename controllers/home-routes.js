a86777296cd608fc17efbd31373ec5454aaef170
Chrisp1993 <chrispollio93@gmail.com>
2022-07-10 12:51:28 -0400

    ready to start adding user authentication

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,4 +38,6 @@ router.get('/', (req, res) => {
       });
   });
 
+  
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,6 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
-
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
