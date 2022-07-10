34e06f8d85691aa1479b20d0cf585c2e418e782d
Chrisp1993 <chrispollio93@gmail.com>
2022-07-09 20:36:26 -0400

    set up the template engine for handlebars, changed the routes folder to controllers and updated require('./routes'); to ('./controllers');

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 //setting up express
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers');
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
