a3380221ab6ae4507bcf4f11b0c79af1f868fb0c
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-07-11 10:37:14 -0400

    controllers dashboard and edit routes

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,22 @@
-//import sequelize constructor
-const Sequelize = require('sequelize');
-
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
-
+//sequelize object with .env config parameter. 
+//logic to run on heroku or local
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: "localhost",
+            dialect: "mysql",
+            port: 3306,
+        }
+    );
 }
 
 module.exports = sequelize;
