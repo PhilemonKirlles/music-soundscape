0c47bf188089072d2802e6e6a2ef961c706d4956
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-07-11 10:46:31 -0400

    editing controllers index, and index-routs

--- controllers/index.js
+++ controllers/index.js
@@ -1,50 +1,22 @@
 const router = require("express").Router();
-const Post = require("../models/Post");
-const User = require("../models/User");
+//api requires can also be done separately
+const apiRoutes = require("./api");
+const indexRoutes = require("./index-routes");
+const loginRoutes = require("./login-routes");
+const postRoutes = require("./post-routes");
+const signupRoutes = require("./signup-routes");
+const logoutRoutes = require("./logout-routes");
+const dashboardRoutes = require("./dashboard-routes");
+const editRoutes = require("./edit-routes");
 
-//index page load
-router.get("/", async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            //joins table
-            include: [
-                {
-                    model: User,
-                    attributes: ["id", "username"],
-                },
-            ],
-            order: [["createdAt", "DESC"]],
-        });
-
-        const posts = postData.map((post) => post.get({ plain: true }));
-        //template for blog package the posts into a custom data structure before rendering
-        const packagedPosts = [];
-        let currentPackage = [];
-        for (let i = 0; i < posts.length; i++) {
-            if (i == 0) {
-                currentPackage.push(posts[i]);
-                packagedPosts.push(currentPackage);
-                currentPackage = [];
-            } else {
-                currentPackage.push(posts[i]);
-            }
-            //if i is odd or if 1 or less left
-            if (i % 2 == 0 || posts.length - i <= 1) {
-                if (currentPackage.length != 0) {
-                    packagedPosts.push(currentPackage);
-                }
-                currentPackage = [];
-            }
-        }
-        //render to page and pass variables for handlebars to work with
-        res.render("index", {
-            loggedIn: req.session.loggedIn,
-            loggedInUserData: req.session.loggedInUserData,
-            posts: packagedPosts,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+//use each of the routes files in the controllers folder
+router.use("/", indexRoutes);
+router.use("/api", apiRoutes);
+router.use("/login", loginRoutes);
+router.use("/post", postRoutes);
+router.use("/signup", signupRoutes);
+router.use("/logout", logoutRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/edit", editRoutes);
 
 module.exports = router;
