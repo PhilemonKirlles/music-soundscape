15bdaf93fa6b9502be7f9fcb8e3a3d953a6d8b3d
Chrisp1993 <chrispollio93@gmail.com>
2022-07-11 13:48:45 -0400

    individual posts are rendering along with the comment window with button and voting button

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,26 +3,9 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
-router.get('/post/:id', (req, res) => {
-    const post = {
-      id: 1,
-      post_url: 'https://handlebarsjs.com/guide/',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      vote_count: 10,
-      comments: [{}, {}],
-      user: {
-        username: 'test_user'
-      }
-    };
-  
-    res.render('single-post', { post });
-  });
-
-  module.exports = router;
 
 // get all posts for homepage
-/*router.get('/', (req, res) => {
+router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
       attributes: [
@@ -50,10 +33,7 @@ router.get('/post/:id', (req, res) => {
       .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
   
-        res.render('homepage', {
-          posts,
-          loggedIn: req.session.loggedIn
-        });
+        res.render('homepage', { posts });
       })
       .catch(err => {
         console.log(err);
@@ -61,53 +41,22 @@ router.get('/post/:id', (req, res) => {
       });
   });
   
-  // get single post
   router.get('/post/:id', (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
+    const post = {
+      id: 1,
+      post_url: 'https://handlebarsjs.com/guide/',
+      title: 'Handlebars Docs',
+      created_at: new Date(),
+      vote_count: 10,
+      comments: [{}, {}],
+      user: {
+        username: 'test_user'
+      }
+    };
   
-        const post = dbPostData.get({ plain: true });
-  
-        res.render('single-post', {
-          post,
-          loggedIn: req.session.loggedIn
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+    res.render('single-post', { post });
   });
-  
+
   router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
@@ -116,6 +65,6 @@ router.get('/post/:id', (req, res) => {
   
     res.render('login');
   });
-  */
-  
+
+  module.exports = router;
   
