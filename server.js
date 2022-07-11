5d6a41d9666a85a52a82ddbffe6b6f5c9cf34197
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-07-11 17:30:42 -0400

    set sessions and const missing dependancy

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // Dependencies
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-
-//missing dependencies err
-const path = requir("path");
+const session = require("express-session");
+const path = require("path");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require("./config/connection");
 const controllers = require("./controllers");
@@ -18,25 +17,35 @@ const PORT = process.env.PORT || 3001;
 
 // Set up sessions
 const sess = {
-    secret: ...,
+    secret: "Secret key goes here",
     cookie: {
         //Note: in milliseconds (86,400,000 === 1 day)
         //57600000 = 16 hours
-    
+        maxAge: 57600000,
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+app.use(session(sess));
 
 //setup handlebars with express
-
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
 
 //allow api to use json and url encoding
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //set public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Sets up the routes
 app.use(controllers);
 
 
-// begin listening with sequelize for db connection
+//Begin listening with sequelize for db connection
 //Note: "force start should be false if using 'npm run seed' to populate and create db as it will recreate tables each server reload"
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log("Now listening: " + PORT));
