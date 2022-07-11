dde307b7afda9b7fe251023965e789a2c6c3fe89
Chrisp1993 <chrispollio93@gmail.com>
2022-07-11 17:32:10 -0400

    working on the voting feature on comments while a user is logged in - causing error 1452 on mysql currently

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,4 +1,4 @@
-async function upvoteClickHandler(event) {
+/*async function upvoteClickHandler(event) {
     event.preventDefault();
   
     const id = window.location.toString().split('/')[
@@ -22,4 +22,36 @@ async function upvoteClickHandler(event) {
   }
   
   document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+  */
+
+  async function upvoteClickHandler(event) {
+  event.preventDefault();
+
+  console.log('button clicked');
+}
+
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+
+async function upvoteClickHandler(event) {
+    event.preventDefault();
   
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+          post_id: id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+  }
