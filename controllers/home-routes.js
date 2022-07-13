2cdd58bde7cd1c4872cfd26c93c74fd56920f428
Chrisp1993 <chrispollio93@gmail.com>
2022-07-13 07:10:33 -0400

    feature/blog-post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
       .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
   
-        res.render('homepage', { posts });
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
@@ -71,7 +71,7 @@ router.get('/', (req, res) => {
       .then(dbPostData => {
           const post = dbPostData.get({plain: true});
         console.log(dbPostData.post)
-        res.render('single-post', { post });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
       })
 
         .catch(err => {
